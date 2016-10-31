94d552fe1007da2a12d7088581b1c9c986a7af7b
hjue <haojue@gmail.com>
2016-10-31 11:16:42 +0800

    deleted unused code

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
-import './demo.scss'
-import data from './data.json'
+import './index.scss';
 
-
-const Profile = ()=>{
-  return <h1>Profile</h1>
-}
-
-const App = React.createClass({
-
-  getInitialState: function() {
-      return {count: this.props.elapsed};
-  },
-
-  render: function() {
-
+class App extends Component {
+  render() {
     return (
-      <div><h1>Hello React!</h1>
-      <Profile />
-      {this.props.elapsed }</div>
+      <div>Hello</div>
     );
   }
-});
+}
 
-var start = new Date().getTime();
-setInterval(function() {
-  render( React.createElement(App,{elapsed: new Date().getTime() - start}), document.getElementById('app'));
-},3000);
+render( <App />, document.getElementById('app'));
